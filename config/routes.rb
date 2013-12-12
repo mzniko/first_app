6dfcb1d2b26f45b9ac6dab488f748106970ec25b
Marko Nikolovski <mz.nikolovski@gmail.com>
2013-12-11 18:56:56 -0800

    route fix pt2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 FirstApp::Application.routes.draw do
-  match '/', to: 'welcome#index', via: 'get'
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +50,7 @@ FirstApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+  root 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
